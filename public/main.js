30766bf14bd7b5c40f849e84ece986f01d306cf3
RubenGtz0 <gutierrezruben.000@gmail.com>
2024-05-09 10:56:25 -0600

    Render de mensajes v1.0.3

--- public/main.js
+++ public/main.js
@@ -10,4 +10,30 @@ var socket= io.connect('http://localhost:3002',{'forceNew':true});
  */
 socket.on('messages', function(data){
     console.log(data);
+    render(data);
 });
+
+/* Creamos un template para que nos imprima el contenido*/
+
+function render (data){
+    //Aqui se inicia el manejo de string que viene en EM6 se usan estas comillas ´´
+    // Las variables se colocan con el signo de $ y entre {}
+    var html = `<div>
+    <strong> ${data.autor}</strong>:
+    <em>${data.texto}</em>
+    </div>`;
+
+    document.getElementById('messages').innerHTML=html;
+}
+//Despues renderizamos los datos actualizados en la consola
+
+//Cada vez que alguien precione el boton enviar en el formulario 
+//El cliente emite un nuevo mensaje y manda el payload
+function addMessage(e){
+    var payload = {
+        autor: document.getElementById(username).value,
+        texto: document.getElementById(texto).value
+    };
+    socket.emit('new-message', payload);
+    return false;
+}
